5037afd45bc97b9443806ddc3d5dc4b734a9a86d
Justin Laptop <justindholmberg@gmail.com>
2025-07-22 15:38:43 -0400

    Major content update for snake game project including moving assets, adjusting containers, and using prism.js to display source code.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,9 @@
+import fs from "fs";
+
 export default function(eleventyConfig) {
+  eleventyConfig.addShortcode("embedJS", function(path) {
+    return `<pre><code class="language-js">${fs.readFileSync(path, "utf8")}</code></pre>`;
+  });
 
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
