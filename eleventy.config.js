6d772f11e7fb9f1690f5b7ea683afed65bb9a38c
Justin Laptop <justindholmberg@gmail.com>
2025-07-23 19:34:17 -0400

    Completed content writing for snake game project, updated assets for it. Added shortcode in config file for embedding repository files. Added output folder to gitignore. Added styles for embedded files.

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,9 @@ export default function(eleventyConfig) {
   eleventyConfig.addShortcode("embedJS", function(path) {
     return `<pre><code class="language-js">${fs.readFileSync(path, "utf8")}</code></pre>`;
   });
-
+  eleventyConfig.addShortcode("figmaEmbed", function(url) {
+    return `<iframe id="snake-figma-embed" src="https://www.figma.com/embed?embed_host=share&url=${url}" allowfullscreen></iframe>`;
+  });
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   
