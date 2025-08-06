679c5b592e11eee811e61f0671ef4421f707abb1
Justin Laptop <justindholmberg@gmail.com>
2025-08-06 18:02:43 -0400

    Huge kilter content update including images, embeds, writing, minor CSS changes.

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,8 +4,9 @@ export default function(eleventyConfig) {
   eleventyConfig.addShortcode("embedJS", function(path) {
     return `<pre><code class="language-js">${fs.readFileSync(path, "utf8")}</code></pre>`;
   });
-  eleventyConfig.addShortcode("figmaEmbed", function(url) {
-    return `<iframe id="snake-figma-embed" src="https://www.figma.com/embed?embed_host=share&url=${url}" allowfullscreen></iframe>`;
+  eleventyConfig.addShortcode("figmaEmbed", function(url, id = "") {
+    const idAttr = id ? `id="${id}"` : "";
+    return `<iframe ${idAttr} class="figma-embed" src="https://www.figma.com/embed?embed_host=share&url=${url}" allowfullscreen></iframe>`;
   });
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
