27433fa285fb7eb5f9bde232a9106c59ca7d5d0f
Justin Desktop <justindholmberg@gmail.com>
2025-08-16 14:25:11 -0400

    Added _data and projects.js for using templates in index.njk. Changed all URL's for hosting.

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,7 +14,8 @@ export default function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "public"
-    }
+      output: "public",
+    },
+    pathPrefix: "/portfolio/"
   };
 }
---
+++ src/_data/projects.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: "PROJECT: KILTER",
+    slug: "project-kilter",
+    img: "/assets/kilter-imgs/kilter-cover-img.png",
+    alt: "Project Kilter UI Landing Page",
+    role: "UX & UI",
+    oneliner: "minimalist spreadsheet application"
+  },
+  {
+    title: "Snake Game",
+    slug: "snake-game",
+    img: "/assets/snake-imgs/snake-cover-img.png",
+    alt: "Snake Game Thumbnail",
+    role: "Front-End Dev & UI",
+    oneliner: "browser-based arcade classic with custom UI"
+  },
+  {
+    title: "FaithSelects.com",
+    slug: "faithselects",
+    img: "/assets/FS-imgs/FS-cover-img.png",
+    alt: "FaithSelects.com Landing Page",
+    role: "UX & UI",
+    oneliner: "website redesign for FaithSelects, a small herbal business"
+  }
+];
