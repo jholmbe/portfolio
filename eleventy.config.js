232a28290678adb718f6e724a090c5500cf3b891
Justin Desktop <justindholmberg@gmail.com>
2025-08-18 13:49:38 -0400

    Added CNAME file and changed config accordingly.

--- eleventy.config.js
+++ eleventy.config.js
@@ -10,6 +10,7 @@ export default function(eleventyConfig) {
   });
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   return {
     dir: {
